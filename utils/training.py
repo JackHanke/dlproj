5f50123035646c1925e09f8f02c625f72d8ee2af
Daniel Plotkin <danmplotkin@gmail.com>
2025-03-04 17:58:23 -0600

    Implemented checkpoint

--- utils/training.py
+++ utils/training.py
@@ -1,5 +1,6 @@
 from utils.networks import DemoNet
 from utils.self_play import SelfPlaySession
+from utils.agent import Agent
 from utils.losses import combined_loss
 from utils.memory import ReplayMemory
 import torch
@@ -7,6 +8,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from typing import Union
+import json
+import os
+import multiprocessing as mp
 
 
 def train_on_batch(
@@ -34,3 +38,54 @@ def train_on_batch(
     # Backward pass
     loss.backward()
     optimizer.step()
+
+
+def _background_compute_elo_and_save(agent: Agent, info_path: str, verbose: str) -> None:
+    if verbose:
+        print("Beginning Elo computation in background process...")
+    agent.compute_bayes_elo()  # This is the slow function
+    # Prepare info to save
+    info = {
+        "version": agent.version,
+        "elo": agent.elo
+    }
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(info_path), exist_ok=True)
+    with open(info_path, "w") as f:
+        json.dump(info, f)
+    if verbose:
+        print(f"Finished Elo computation and saved info to {info_path}")
+
+
+class Checkpoint:
+    def __init__(self, verbose: bool):
+        self.verbose = verbose
+        self.best_agent = None
+        self.best_weights = None
+        self.elo = None
+        self.version = None
+        self.iteration = None
+
+    def save_state_dict(self, path: str) -> None:
+        # Create directory if it doesn't exist
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        torch.save(self.best_weights, path)
+        if self.verbose:
+            print(f"Saved new best weights to {path}")
+
+    def step(self, weights_path: str, info_path: str, current_best_agent):
+        if current_best_agent.version > self.version:
+            self.save_state_dict(path=weights_path)
+            self.best_agent = current_best_agent
+            self.best_weights = current_best_agent.network.state_dict()
+            self.version = current_best_agent.version
+
+            if self.verbose:
+                print("Starting background process for Elo computation...")
+
+            # Start the Elo computation in a separate process
+            p = mp.Process(
+                target=_background_compute_elo_and_save,
+                args=(current_best_agent, info_path, self.verbose)
+            )
+            p.start() # Will run in background since we dont have a .join()
