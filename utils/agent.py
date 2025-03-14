6c71848912535d471408d235c2e0b8f86571b96f
JackHanke <jack.hanke98@gmail.com>
2025-03-14 14:55:03 -0500

    stockfish starter

--- utils/agent.py
+++ utils/agent.py
@@ -38,13 +38,14 @@ class Agent:
         raise NotImplementedError
 
 class Stockfish:
-    def __init__(self, level):
+    def __init__(self, level, move_time):
         # Sotckfish 5: https://stockfishchess.org/blog/2014/stockfish-5/
         self.version = "Stockfish"
         self.network = None
         self.sims = None
         self.nod_cache = None
         self.level = level
+        self.move_time = move_time
         if platform == 'darwin': stock_path = f'./stockfish-5-mac/Mac/stockfish-5-64'
         elif platform == 'linux': stock_path = f'./stockfish-5-mac/src/stockfish' 
         self.engine = chess.engine.SimpleEngine.popen_uci(stock_path)
@@ -53,13 +54,13 @@ class Stockfish:
     def inference(self, board_state: chess.Board, observation: torch.tensor, device: torch.device, tau: float = 1):
         # fuck you PettingZoo
         if board_state.turn: # it white's turn
-            uci_move = str(self.engine.play(board_state, chess.engine.Limit(time=1.0)).move)
+            uci_move = str(self.engine.play(board_state, chess.engine.Limit(time=self.move_time)).move)
         elif not board_state.turn: # if black's turn
             mirrored_board = board_state.mirror()
-            uci_move = str(self.engine.play(mirrored_board, chess.engine.Limit(time=1.0)).move)
+            uci_move = str(self.engine.play(mirrored_board, chess.engine.Limit(time=self.move_time)).move)
         _ = legal_moves(orig_board=board_state) # idk if this is necessary
         action = moves_to_actions[uci_move]
-        value = 1 # NOTE compatability
+        value = 0 # NOTE compatability
         return action, value
 
     def compute_bayes_elo(self, *args):
